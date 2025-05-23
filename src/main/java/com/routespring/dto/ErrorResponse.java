410bec21681ed9ed60ba0a00e916aaf0576a1326
sumitRTS <sumit.kumar@routespring.com>
2025-05-23 15:27:21 +0530

    Initial commit of rs-common Maven project

---
+++ src/main/java/com/routespring/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.routespring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorResponse {
+	private String errorCode;
+	private String message;
+	private String details;
+}
