60851731b64205a610ae253e6fa8debf63f793f2
sumitRTS <sumit.kumar@routespring.com>
2025-05-24 20:15:13 +0530

    adding car objects

--- src/main/java/com/routespring/dto/CarSearch.java
+++ src/main/java/com/routespring/dto/CarSearch.java
@@ -1,5 +1,7 @@
 package com.routespring.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,25 +13,35 @@ import lombok.Setter;
 @Setter
 public class CarSearch {
 
+	@JsonProperty("pick_lat")
 	private String latitudePickUp;
+	@JsonProperty("pick_long")
 	private String longitudePickUp;
 
+	@JsonProperty("drop_lat")
 	private String latitudeDropOff;
+	@JsonProperty("drop_long")
 	private String longitudeDropOff;
 
+	@JsonProperty("dropLocation")
 	private String dropLocation;
+	@JsonProperty("pickLocation")
 	private String pickLocation;
 
+	@JsonProperty("pickDate")
 	private String pickUpDate;
+	@JsonProperty("pickTime")
 	private String pickupTime;
 
+	@JsonProperty("dropDate")
 	private String dropoffDate;
+	@JsonProperty("dropTime")
 	private String dropoffTime;
 
 	private String dropOffType;
 	private String pickupType;
 	private boolean isSameLocation;
-	
+
 	private String travellerEmail;
 
 }
